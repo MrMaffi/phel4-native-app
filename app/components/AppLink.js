532d08f6bc88d9ee042743f8581a9db92405e6fe
MrMaffi <mrmaffi550@gmail.com>
2021-03-29 08:47:07 +0300

    refactor(AppLink): add font color style prop
    Added color property to styles.link object. Color prop value changed
    from default -> '#454545'.
    
    Closes: #25

--- app/components/AppLink.js
+++ app/components/AppLink.js
@@ -3,6 +3,8 @@ import { StyleSheet, TouchableOpacity } from 'react-native';
 
 import AppText from './AppText';
 
+import colors from '../config/colors';
+
 export default function AppLink({ children, onPress, style }) {
   return (
     <TouchableOpacity onPress={onPress}>
@@ -13,6 +15,7 @@ export default function AppLink({ children, onPress, style }) {
 
 const styles = StyleSheet.create({
   link: {
+    color: colors.darkGray,
     fontSize: 18,
     textDecorationLine: 'underline',
   },
