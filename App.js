8f42a2422e668e9b8328b765a540c51862251d39
MrMaffi <mrmaffi550@gmail.com>
2021-03-30 10:00:44 +0300

    Revert "Merge pull request #36 from MrMaffi/screens"
    This reverts commit 47fc731904555eda70883b1495591101b27186fa, reversing
    changes made to a7a4461d8759e2166505bc1916cdead4e9dfe793.

--- App.js
+++ App.js
@@ -1,24 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import AppLoading from 'expo-app-loading';
-import {
-  useFonts,
-  Nunito_400Regular,
-  Nunito_600SemiBold,
-  Nunito_700Bold,
-} from '@expo-google-fonts/nunito';
-
-import LoginScreen from './app/screens/LoginScreen';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  let [fontsLoaded] = useFonts({
-    Nunito_400Regular,
-    Nunito_600SemiBold,
-    Nunito_700Bold,
-  });
-
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
-
-  return <LoginScreen />;
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- app/config/colors.js
+++
@@ -1,9 +0,0 @@
-export default {
-  primary: '#f2f6ff',
-  secondary: '#2e66fd',
-  black: '#000',
-  white: '#fff',
-  darkGray: '#454545',
-  lightGray: '#bebebe',
-  danger: '#ff2a2a',
-};
