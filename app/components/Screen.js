824ebabe1149ca389df0a5630583af094a967d89
MrMaffi <mrmaffi550@gmail.com>
2021-04-13 14:57:50 +0300

    refactor(Screen): redesign the component structure
    All component elements were wrapped by the SafeAreaView component.
    
    Removed top and bottom paddings, violating the rule of mass reuse of a
    component.
    
    Closes: #71

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import Constants from 'expo-constants';
-import { Dimensions, ScrollView, StyleSheet, View } from 'react-native';
+import {
+  Dimensions,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  View,
+} from 'react-native';
 
 import colors from '../config/colors';
 
 export default function Screen({ children, style }) {
   return (
-    <ScrollView style={styles.screen}>
-      <View style={[styles.container, style]}>{children}</View>
-    </ScrollView>
+    <SafeAreaView style={styles.screen}>
+      <ScrollView>
+        <View style={[styles.container, style]}>{children}</View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
     backgroundColor: colors.primary,
-    paddingHorizontal: 20,
   },
   container: {
     minHeight: Dimensions.get('window').height,
-    paddingTop: Constants.statusBarHeight + 30,
-    paddingBottom: 60,
+    paddingTop: Platform.OS !== 'ios' ? Constants.statusBarHeight : null,
+    paddingHorizontal: 20,
   },
 });
